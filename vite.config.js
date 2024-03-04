bd4c47eb461cd8118d38e4b7d6a390586b78b8a2
florisflorisw <florisflorisw@hotmail.com>
2024-03-04 22:08:35 +0100

    Removed traces old lib

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import transformer from './node_modules/my-library/plugins/svelteTransformer';
-import server from './node_modules/my-library/plugins/server';
 
 export default defineConfig({
 	plugins: [
-		transformer(),
-		server(),
 		sveltekit()
 	]
 });
